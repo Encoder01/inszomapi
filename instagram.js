3f5b93be8032bdf4ad43f69f5da411e23d7cf5ef
Huzeyfe <jungar.kagan@gmail.com>
2021-08-28 10:44:18 +0300

    asf

--- instagram.js
+++ instagram.js
@@ -1,7 +1,7 @@
 const Instagram = require('instagram-web-api')
 const FileCookieStore = require('tough-cookie-filestore2')
 const cookieStore = new FileCookieStore('./cookies.json')
-const username = "squaking02";
+const username = "maximus.decimus.meridiuss";
 const password = "40045052339";
 const client = new Instagram({ username, password, cookieStore });
 
