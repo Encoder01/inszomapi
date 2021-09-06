6848181ca9efe9f38e6cd3b095478799f894d12a
Huzeyfe <jungar.kagan@gmail.com>
2021-09-06 11:20:35 +0300

    jdhdfh

--- instagram.js
+++ instagram.js
@@ -1,7 +1,7 @@
 const Instagram = require('instagram-web-api')
 const FileCookieStore = require('tough-cookie-filestore2')
 const cookieStore = new FileCookieStore('./cookies.json')
-const username = "squaking01";
+const username = "batoso7096";
 const password = "40045052339";
 const client = new Instagram({ username, password, cookieStore });
 
